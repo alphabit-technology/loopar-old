0301158caed192e40d468d5456709c0e63ceaef8
Quantum Bit Core <qubitcore.io@gmail.com>
2022-10-20 20:32:41 -0400

    Add Recovery Form
    Add Recovery Form

---
+++ apps/loopar/modules/auth/reset-password/reset-password-controller.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+import {BaseController} from 'loopar-env';
+
+export default class ResetPasswordController extends BaseController {
+    constructor(props){
+        super(props);
+    }
+}
---
+++ apps/loopar/modules/auth/reset-password/reset-password.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+import {BaseDocument} from 'loopar-env';
+
+export default class ResetPassword extends BaseDocument {
+    constructor(props){
+        super(props);
+    }
+}
